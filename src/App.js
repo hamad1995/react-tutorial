86e12dddbdf2cd8d3b9bffae45a09a62f530708d
Hammad AlHammad <h.alhammad@tamkeentech.sa>
2022-04-08 12:53:16 +0300

    trying useState function

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
-
+import {useState, useEffect} from 'react'
 function App() {
+  const [count, setCount]= useState(0)
+  const [name, setName] = useState('')
+
+
+useEffect(()=>{
+  document.title = `you clicked ${count}`
+})
+
+
   return (
     <div className="App">
+      <p>you clicked the button {count} times !</p>
+      <button onClick={()=>setCount(count +1)}>click</button>
+      <label >enter a name</label>
+      <input type="text" onChange={()=>setName('hamad')}/>
+      <input type="submit" />
+      <h3>hello {name}</h3>
+      <br />
       <h1>hello react </h1>
     </div>
   );
